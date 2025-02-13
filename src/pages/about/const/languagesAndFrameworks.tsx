cd41493f1fce6706e075a1a13a9dd5381a6392b7
Athul <athuljoseliju123@gmail.com>
2025-02-13 20:50:04 +1100

    Your commit message here

--- src/pages/about/const/languagesAndFrameworks.tsx
+++ src/pages/about/const/languagesAndFrameworks.tsx
@@ -1,6 +1,6 @@
 // Languages and Frameworks data
 const languagesAndFrameworks = [
- '.../assets/images/java.svg',
+ '../././assets/images/java.svg',
   '/assets/images/cpp.svg',
   '/assets/images/html.svg',
   '/assets/images/css.svg',
