ab4bbe2312de1d88760103923d6e0c14d866608f
Athul <athuljoseliju123@gmail.com>
2025-02-17 16:54:52 +1100

    images about added

--- src/pages/about/const/languagesAndFrameworks.tsx
+++ src/pages/about/const/languagesAndFrameworks.tsx
@@ -1,21 +1,39 @@
-// Languages and Frameworks data
-const languagesAndFrameworks = [
- '../../../../assets/images/java.svg',
-  '././././assets/images/cpp.svg',
-  './././assets/images/html.svg',
-  '././assets/images/css.svg',
-  './assets/images/python.svg',
-  '../../../assets/images/react.svg',
-  '../assets/images/sql.svg',
-  '../../assets/images/mui.svg',
-  '/assets/images/node-js.svg',
-  '/assets/images/js.svg',
-  '/assets/images/next.svg',
-  '/assets/images/postgre.svg',
-  '/assets/images/slack.svg',
-  '/assets/images/ps.svg',
-  '/assets/images/ai.svg',
-  '/public/figma.svg',
-  ];
+// Import images from assets
+import javaLogo from '../../../assets/java.svg';
+import cppLogo from '../../../assets/cpp.svg';
+import htmlLogo from '../../../assets/html.svg';
+import cssLogo from '../../../assets/css.svg';
+import pythonLogo from '../../../assets/python.svg';
+import reactLogo from '../../../assets/react.svg';
+import sqlLogo from '../../../assets/sql.svg';
+import muiLogo from '../../../assets/mui.svg';
+import nodeLogo from '../../../assets/node-js.svg';
+import jsLogo from '../../../assets/js.svg';
+import nextLogo from '../../../assets/next.svg';
+import postgreLogo from '../../../assets/postgre.svg';
+import slackLogo from '../../../assets/slack.svg';
+import psLogo from '../../../assets/ps.svg';
+import aiLogo from '../../../assets/ai.svg';
+import figmaLogo from '../../../assets/figma.svg';
 
-  export default languagesAndFrameworks;
+// Store in an array
+const languagesAndFrameworks = [
+  javaLogo,
+  cppLogo,
+  htmlLogo,
+  cssLogo,
+  pythonLogo,
+  reactLogo,
+  sqlLogo,
+  muiLogo,
+  nodeLogo,
+  jsLogo,
+  nextLogo,
+  postgreLogo,
+  slackLogo,
+  psLogo,
+  aiLogo,
+  figmaLogo
+];
+
+export default languagesAndFrameworks;
